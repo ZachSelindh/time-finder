2d371993f34db0acd838928902d06a14b9d5144e
zachSelindh <zachselindh@gmail.com>
2022-01-21 09:49:32 -0600

    +'d service worker

--- src/index.js
+++ src/index.js
@@ -7,6 +7,7 @@ import { ThemeProvider } from '@mui/material/styles';
 import AdapterDateFns from '@mui/lab/AdapterDateFns';
 import LocalizationProvider from '@mui/lab/LocalizationProvider';
 import { theme } from './theme';
+import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 
 ReactDOM.render(
   <React.StrictMode>
@@ -23,3 +24,4 @@ ReactDOM.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+serviceWorkerRegistration.register();
